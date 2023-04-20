b9519fef395c3d4498f28a67ea3353f824ff8550
Akshay <akshaykant5006@gmail.com>
2023-04-20 11:53:40 +0530

    adding constent code in config.js

--- src/components/CafeCard.js
+++ src/components/CafeCard.js
@@ -1,4 +1,4 @@
-
+import { IMG_CDN_URL } from "../Config";
 
 const CafeCards = ({name,cuisines,totalRatingsString,cloudinaryImageId}) => {
   
@@ -6,7 +6,7 @@ const CafeCards = ({name,cuisines,totalRatingsString,cloudinaryImageId}) => {
       <div className="cards">
         <div>
           <img
-            src={`https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/${cloudinaryImageId}`}
+            src={IMG_CDN_URL + cloudinaryImageId}
             alt="burger king"
           />
         </div>
