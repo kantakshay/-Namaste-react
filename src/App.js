2a6dfbb523feecdf635fba6ffaedc8f218b96e15
Akshay <akshaykant5006@gmail.com>
2023-04-22 14:07:19 +0530

    learned about states,search & filter functionality

--- src/App.js
+++ src/App.js
@@ -1,18 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
-import Body from './components/Body';
+import Body from "./components/Body";
 import Footer from "./components/Footer";
-
-
-
-
-
-
-
-
-
-
 
 const AppLayout = () => {
   return (
--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,46 @@
 import CafeCards from "./CafeCard";
 import { cardContent } from "../Config";
+import { useState } from "react";
 
-const Body = () => (
-    <div className="cafelist">
-       {cardContent.map((cardContent)=>{
-      return  <CafeCards {...cardContent.data} key={cardContent.data.id}/>
-       })}
-    </div>
+
+const fliterData = (searchInput,restaurants)=>{
+  const filteredData = restaurants.filter((restaurant) => 
+     restaurant.data.name.includes(searchInput) );
+  return filteredData;
+}
+
+
+const Body = () => {
+  const [searchInput, setSearchInput] = useState("");
+  const [restaurants,setRestaurant] = useState(cardContent)
+  
+  return (
+    <>
+      <div>
+        <input
+          type="text"
+          className="search-input"
+          placeholder="Search"
+          value={searchInput}
+          onChange={(e) => {
+            setSearchInput(e.target.value);
+          }}
+        />
+        <button onClick={()=>{
+           const data =    fliterData(searchInput,restaurants);
+
+           setRestaurant(data);
+        }
+     
+        }>search</button>
+      </div>
+      <div className="cafelist">
+        {restaurants.map((cardContent) => {
+          return <CafeCards {...cardContent.data} key={cardContent.data.id} />;
+        })}
+      </div>
+    </>
   );
+};
 
-  export default Body;
+export default Body;
